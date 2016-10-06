d810197df21cd4afc15d928ab410451c88c4af6a
Isuru Weerarathna <iweerarathna@virtusa.com>
2016-10-06 11:24:31 +0530

    dependency visualized

--- tests/src/main/java/Deps.java
+++ tests/src/main/java/Deps.java
@@ -1,8 +1,14 @@
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.implementations.SingleGraph;
 
 import java.io.File;
 import java.nio.file.Path;
 import java.util.*;
+import java.util.function.BiConsumer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,11 +18,42 @@ import java.util.regex.Pattern;
 public class Deps {
 
     public static void main(String[] args) throws Exception {
-        File dir = new File("C:\\Projects\\insight5\\nyql\\examples");
-        Map<String, Set<String>> calls = new HashMap<>();
-        scan(dir, dir, calls);
+        System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
 
-        System.out.println(calls);
+        File dir = new File("C:\\Projects\\insight5\\nyql\\examples");
+//        Map<String, Set<String>> calls = new HashMap<>();
+//        scan(dir, dir, calls);
+//
+//        System.out.println(calls);
+        printDependenciesOf(dir, "");
+    }
+
+    public static void printDependenciesOf(File scriptDir, String scriptId) throws Exception {
+        Map<String, Set<String>> calls = new HashMap<>();
+        scan(scriptDir, scriptDir, calls);
+
+        Graph g = createGraph(calls);
+        g.display();
+    }
+
+    private static Graph createGraph(Map<String, Set<String>> map) throws Exception {
+        Graph g = new SingleGraph("nyql", false, true);
+
+        map.forEach((s, strings) -> {
+            if (strings.size() > 0) {
+                strings.forEach(other -> {
+                    Edge edge = g.addEdge(s + other, s, other, true);
+                    edge.getSourceNode().setAttribute("label", s);
+                    edge.getTargetNode().setAttribute("label", other);
+                });
+            } else {
+                g.addNode(s).setAttribute("label", s);
+            }
+        });
+
+
+        g.addAttribute("ui.stylesheet", "node { text-size: 12; }");
+        return g;
     }
 
     private static void scan(File src, File baseDir, Map<String, Set<String>> calls) throws Exception {
@@ -30,6 +67,7 @@ public class Deps {
         } else {
             Set<String> toCalls = findCalls(src);
             String relPath = baseDir.toPath().relativize(src.toPath()).toString().replace("\\", "/");
+            relPath = StringUtils.substringBeforeLast(relPath, ".");
 
             System.out.println("Scanning: " + relPath);
             System.out.println("   Called: " + toCalls);
