a3fa5daf9064d96fda93742a1ce180ce7d59eb6b
Isuru Weerarathna <iweerarathna@virtusa.com>
2016-10-24 12:50:08 +0530

    added test case for insert table

--- tests/scripts/inserts/basic.groovy
+++ tests/scripts/inserts/basic.groovy
@@ -1,6 +1,20 @@
 /**
  * @author IWEERARATHNA
  */
+def innQ = $DSL.select {
+    TARGET (Film.alias("f"))
+    WHERE {
+        EQ (f.film_id, NUM(1))
+    }
+}
+
+def innQP = $DSL.select {
+    TARGET (Film.alias("f"))
+    WHERE {
+        EQ (f.film_id, PARAM("id"))
+    }
+}
+
 [
         $DSL.insert {
             TARGET (Film.alias("f"))
@@ -32,4 +46,23 @@
         ["INSERT INTO `Film` (`film_id`, `title`) VALUES (?, ?)",
          ["id", "title"]],
 
+        $DSL.insert {
+            TARGET (Film.alias("f"))
+            DATA (
+                    "film_id": TABLE(innQ),
+                    "title": PARAM("title")
+            )
+            RETURN_KEYS()
+        },
+        ["INSERT INTO `Film` (`film_id`, `title`) VALUES ((SELECT * FROM `Film` f WHERE f.film_id = 1), ?)", ["title"]],
+
+        $DSL.insert {
+            TARGET (Film.alias("f"))
+            DATA (
+                    "film_id": TABLE(innQP),
+                    "title": PARAM("title")
+            )
+            RETURN_KEYS()
+        },
+        ["INSERT INTO `Film` (`film_id`, `title`) VALUES ((SELECT * FROM `Film` f WHERE f.film_id = ?), ?)", ["id", "title"]],
 ]
