09f6f0eef4db2aa446fac7817e6de3a76c8a04b6
Isuru Weerarathna <iweerarathna@virtusa.com>
2016-11-02 15:20:44 +0530

    set auto commit again

--- engine/engine/src/main/groovy/com/virtusa/gto/nyql/engine/impl/QJdbcExecutor.groovy
+++ engine/engine/src/main/groovy/com/virtusa/gto/nyql/engine/impl/QJdbcExecutor.groovy
@@ -150,7 +150,7 @@ class QJdbcExecutor implements QExecutor {
         PreparedStatement statement = null
         try {
             statement = getConnection().prepareStatement(script.proxy.query)
-            //connection.setAutoCommit(false)
+            getConnection().setAutoCommit(false)
 
             List<AParam> parameters = script.proxy.orderedParameters
             Object batchData = script.qSession.sessionVariables['batch']
@@ -167,13 +167,11 @@ class QJdbcExecutor implements QExecutor {
             }
 
             int[] counts = statement.executeBatch()
-            //connection.commit()
+            getConnection().commit()
             return [count: counts]
 
         } finally {
-            //if (connection != null) {
-            //    connection.setAutoCommit(true)
-            //}
+            getConnection().setAutoCommit(true)
 
             if (statement != null) {
                 statement.close()
