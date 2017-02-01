c99e333170b9cea022abaee959e9941582ccdc8d
Isuru Weerarathna <uisurumadushanka89@gmail.com>
2017-02-01 14:21:24 +0530

    publish.sh permissions set in travis

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
-    mvn clean install deploy -Possrh --settings $GPG_DIR/settings.xml
+    mvn deploy -Possrh --settings $GPG_DIR/settings.xml
     exit $?
 fi
