c42f45ba6a1982d8af601febc58c8e6a10b7cf5f
Isuru Weerarathna <uisurumadushanka89@gmail.com>
2017-02-18 08:09:45 +0530

    nyserver log4j externalized

--- tests/scripts/updates/specials.groovy
+++ tests/scripts/updates/specials.groovy
@@ -21,4 +21,14 @@
         [
             mysql: "UPDATE `Address` ad SET ad.address2 = CASE WHEN ad.address2 IS NOT NULL THEN \"\" ELSE ad.address2 END"
         ],
+
+        $DSL.update {
+            TARGET (UserVisits.alias("uv"))
+            SET {
+                EQ (uv.visits, uv.visits + 1)
+            }
+        },
+        [
+                mysql: "UPDATE `UserVisits` uv SET uv.visits = (uv.visits + 1)"
+        ],
 ]
